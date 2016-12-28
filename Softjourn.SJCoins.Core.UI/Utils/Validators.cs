3d7c92a9e4c68365de036767b8a7c138546caa19
Oleksandr Martynets <omartynets@softjourn.com>
2016-12-28 16:15:28 +0200

    Added LoginPresenter, interfaces for LoginPresenter and LoginView.

---
+++ Softjourn.SJCoins.Core.UI/Utils/Validators.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Softjourn.SJCoins.Core.UI.Utils
+{
+    public class Validators
+    {
+        public static int ValidateCredentials(string userName, string password)
+        {
+            const int fieldsAreAmpty = 1;
+            const int userNameNotValid = 2;
+            const int passwordNotValid = 3;
+            const int credentialsAreValid = -1;
+
+            if (password.Length< 1 && userName.Length< 1)
+            {
+                return fieldsAreAmpty;
+            }
+
+            if (userName.Length< 1 || !Regex.IsMatch(userName, "[a-z]+"))
+            {
+                return userNameNotValid;
+            }
+
+            if (password.Length< 1)
+            {
+                return passwordNotValid;
+            }
+
+            return credentialsAreValid;
+        }
+    }
+}
