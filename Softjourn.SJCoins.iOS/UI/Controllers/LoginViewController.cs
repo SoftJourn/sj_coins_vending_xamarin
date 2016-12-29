2228d8e7de5d1fdf281d9b73c94faa25ac6479fd
Oleg Pankiv <oleg.pankiv@gmail.com>
2016-12-29 17:48:25 +0200

    working on loginview controller implementation

--- Softjourn.SJCoins.iOS/UI/Controllers/LoginViewController.cs
+++ Softjourn.SJCoins.iOS/UI/Controllers/LoginViewController.cs
@@ -1,20 +1,25 @@
 using Foundation;
 using System;
 using UIKit;
+using Softjourn.SJCoins.Core.UI.ViewInterfaces;
+using Softjourn.SJCoins.Core.UI.Presenters;
 
 namespace Softjourn.SJCoins.iOS.UI.Controllers
 {
-    public partial class LoginViewController : UIViewController
-    {
-		//Constructor
-        public LoginViewController (IntPtr handle) : base (handle)
-        {
-        }
+	public partial class LoginViewController : UIViewController, ILoginView
+	{
+		private LoginPresenter _loginPresenter;
 
-		//Life cycle
+		//Constructor
+		public LoginViewController(IntPtr handle) : base(handle)
+		{
+		}
+	
+		//Life cyclee
 		public override void ViewDidLoad()
 		{
 			base.ViewDidLoad();
+			_loginPresenter = new LoginPresenter(this);
 		}
 
 		public override void ViewWillAppear(bool animated)
@@ -29,6 +34,49 @@ namespace Softjourn.SJCoins.iOS.UI.Controllers
 		partial void LoginButton_TouchUpInside(UIButton sender)
 		{
 			Console.WriteLine("sjdhfjkshgjksdhgjksdhgjkhdsjkghdsjkghdjksghjk sdgsdhgjkdhsgjk");
+		}
+
+		//ILoginView Interface
+		public void HideProgress()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void NavigateToMain()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void NavigateToWelcome()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SetPasswordError()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SetUsernameError()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ShowMessage(string message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ShowNoInternetError()
+		{
+			UIAlertController alert = UIAlertController.Create("Warning", "No internet connecton.", UIAlertControllerStyle.Alert);
+			alert.AddAction(UIAlertAction.Create("Ok", UIAlertActionStyle.Cancel, null));
+			this.PresentViewController(alert, animated: true, completionHandler: null);
+		}
+
+		public void ShowProgress(string message)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
