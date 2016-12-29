0a2711143784ccfdd080665b1829c8940b960cf2
Oleksandr Martynets <omartynets@softjourn.com>
2016-12-29 18:45:05 +0200

    Added SplashPresenter fubnctionlaity

---
+++ Softjourn.SJCoins.Core.UI/Presenters/LaunchPresenter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Connectivity;
+using Softjourn.SJCoins.Core.UI.Presenters.IPresenters;
+using Softjourn.SJCoins.Core.UI.ViewInterfaces;
+
+namespace Softjourn.SJCoins.Core.UI.Presenters
+{
+    public class LaunchPresenter : ILaunchPresenter
+    {
+
+        private ILaunchView _view;
+
+        public LaunchPresenter(ILaunchView view)
+        {
+            _view = view;
+        }
+
+        public void ChooseStartPage()
+        {
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                if (Helpers.Settings.FirstLaunch)
+                {
+                    _view.ToWelcomePage();
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(Helpers.Settings.AccessToken))
+                    {
+                        _view.ToLoginPage();
+                    }
+                    else
+                    {
+                        _view.ToMainPage();
+                    }
+                }
+            }
+            else
+            {
+                _view.NoInternet();
+            }
+        }
+    }
+}
--- Softjourn.SJCoins.Core/Helpers/Settings.cs
+++ Softjourn.SJCoins.Core/Helpers/Settings.cs
@@ -4,49 +4,58 @@ using Plugin.Settings.Abstractions;
 
 namespace Softjourn.SJCoins.Core.Helpers
 {
-  /// <summary>
-  /// This is the Settings static class that can be used in your Core solution or in any
-  /// of your client applications. All settings are laid out the same exact way with getters
-  /// and setters. 
-  /// </summary>
-  public static class Settings
-  {
-    private static ISettings AppSettings
+    /// <summary>
+    /// This is the Settings static class that can be used in your Core solution or in any
+    /// of your client applications. All settings are laid out the same exact way with getters
+    /// and setters. 
+    /// </summary>
+    public static class Settings
     {
-      get
-      {
-        return CrossSettings.Current;
-      }
+        private static ISettings AppSettings
+        {
+            get
+            {
+                return CrossSettings.Current;
+            }
+        }
+
+        #region Setting Constants
+
+        private const string SettingsKey = "settings_key";
+        private static readonly string SettingsDefault = string.Empty;
+
+        private const string IsFirstLaunch = "is_first_launch";
+        private static readonly bool IsFirstLaunchDefault = true;
+
+        private const string AccessTokenKey = "access_token_key";
+        private static readonly string AccessTokenDefault = string.Empty;
+
+        #endregion
+
+
+        public static string GeneralSettings
+        {
+            get
+            {
+                return AppSettings.GetValueOrDefault<string>(SettingsKey, SettingsDefault);
+            }
+            set
+            {
+                AppSettings.AddOrUpdateValue<string>(SettingsKey, value);
+            }
+        }
+
+        public static bool FirstLaunch
+        {
+            get { return AppSettings.GetValueOrDefault<bool>(IsFirstLaunch, IsFirstLaunchDefault); }
+            set { AppSettings.AddOrUpdateValue(IsFirstLaunch, value); }
+        }
+
+        public static string AccessToken
+        {
+            get { return AppSettings.GetValueOrDefault<string>(AccessTokenKey, AccessTokenDefault); }
+            set { AppSettings.AddOrUpdateValue(AccessTokenKey, value); }
+        }
+
     }
-
-    #region Setting Constants
-
-    private const string SettingsKey = "settings_key";
-    private static readonly string SettingsDefault = string.Empty;
-
-    private const string IsFirstLaunch = "is_first_launch";
-    private static readonly bool IsFirstLaunchDefault = true;
-
-    #endregion
-
-
-    public static string GeneralSettings
-    {
-      get
-      {
-        return AppSettings.GetValueOrDefault<string>(SettingsKey, SettingsDefault);
-      }
-      set
-      {
-        AppSettings.AddOrUpdateValue<string>(SettingsKey, value);
-      }
-    }
-
-      public static bool FirstLaunch
-      {
-          get { return AppSettings.GetValueOrDefault<bool>(IsFirstLaunch, IsFirstLaunchDefault); }
-          set { AppSettings.AddOrUpdateValue(IsFirstLaunch, value); }  
-      }
-
-  }
 }
