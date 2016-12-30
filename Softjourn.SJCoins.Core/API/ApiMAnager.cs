1662a8700542c5eb14d1f3f2083060e4e39c1dfc
Oleksandr Martynets <omartynets@softjourn.com>
2016-12-30 11:10:16 +0200

    Added missed proj files

---
+++ Softjourn.SJCoins.Core/API/ApiMAnager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Softjourn.SJCoins.Core.API.Coins;
+using Softjourn.SJCoins.Core.API.OAuth;
+using Softjourn.SJCoins.Core.API.Vending;
+
+namespace Softjourn.SJCoins.Core.API
+{
+
+    public class ApiManager
+    {
+        private static ApiManager _apiManager;
+        private static OAuthApiClient _authApiClient;
+        private static CoinsApiClient _coinsApiClient;
+        private static VendingApiClient _vendingApiClient;
+
+        public static ApiManager GetInstance()
+        {
+            if (_apiManager == null)
+            {
+                _apiManager = new ApiManager();
+                _authApiClient = new OAuthApiClient();
+                _coinsApiClient = new CoinsApiClient();
+                _vendingApiClient = new VendingApiClient();
+            }
+            return _apiManager;
+        }
+
+        public IOAuthApiProvider GetOauthApiProvider()
+        {
+            return _authApiClient;
+        }
+
+        public IVendingApiProvider GetVendingProcessApiProvider()
+        {
+            return _vendingApiClient;
+        }
+
+        public ICoinsApiProvider GetCoinsApiProvider()
+        {
+            return _coinsApiClient;
+        }
+    }
+}
---
+++ Softjourn.SJCoins.Core/API/Vending/VendingApiClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softjourn.SJCoins.Core.API.Vending
+{
+    public class VendingApiClient : IVendingApiProvider
+    {
+    }
+}
