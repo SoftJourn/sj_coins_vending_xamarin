3ee34a9e0731cb4ce5c16819c3c8cf7019a8718a
Andruhin <Andruha83>
2017-01-16 14:34:12 +0200

    Changed IBasePresenter interface. Added INavigationServise interface.

---
+++ Softjourn.SJCoins.Core.UI/Services/Navigation/NavigationPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softjourn.SJCoins.Core.UI.Services.Navigation
+{
+    public enum NavigationPage
+    {
+        Launch, Welcome, Login
+    }
+}
