813526a4aa4c868478cc05ec8b45f736f2f523e9
Andruhin <Andruha83>
2017-01-23 13:31:43 +0200

    Added base methods for SelectMachinePresenter and ISelectMachineView. Added networt calls for all machine services.

---
+++ Softjourn.SJCoins.Core.UI/ViewInterfaces/ISelectMachineView.cs
@@ -0,0 +1,22 @@
+﻿using Softjourn.SJCoins.Core.API.Model.Machines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Softjourn.SJCoins.Core.UI.ViewInterfaces
+{
+    public interface ISelectMachineView : IBaseView
+    {
+        void ShowProgress(string message);
+
+        void HideProgress();
+
+        void ShowNoMachineView();
+
+        void ShowMachinesList(List<Machines> list);
+
+       
+    }
+}
--- Softjourn.SJCoins.Core/API/Model/AccountInfo/Account.cs
+++ Softjourn.SJCoins.Core/API/Model/AccountInfo/Account.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 
 namespace Softjourn.SJCoins.Core.API.Model.AccountInfo
 {
-    class Account
+    public class Account
     {
         [JsonProperty("amount")]
         private int Amount { get; set; }
