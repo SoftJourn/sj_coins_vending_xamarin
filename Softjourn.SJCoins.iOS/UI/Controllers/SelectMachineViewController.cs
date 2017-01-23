49adbab71949ec37c877abd82f038cc8168beeac
Oleg Pankiv <oleg.pankiv@gmail.com>
2017-01-23 13:33:17 +0200

    save point

---
+++ Softjourn.SJCoins.iOS/UI/Controllers/SelectMachineViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Foundation;
+
+namespace Softjourn.SJCoins.iOS
+{
+	[Register("SelectMachineViewController")]
+	public class SelectMachineViewController : BaseViewController<SelectMachinePresenter>, ISelectMachineView
+	{
+		#region Constructor
+		public SelectMachineViewController(IntPtr handle) : base(handle)
+		{
+		}
+		#endregion
+
+		#region Controller Life cycle 
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+		
+			//Presenter. call method get list
+		}
+		#endregion
+
+		#region ISelectMachineView implementation
+		#endregion
+
+
+
+	}
+}
