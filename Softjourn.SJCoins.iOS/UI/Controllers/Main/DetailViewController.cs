e4ed4dbfa5e53ef9928e5c430539c962a5f3bc19
Oleg Pankiv <oleg.pankiv@gmail.com>
2017-02-09 14:19:17 +0200

    implemented navigation to detailViewController from home page

--- Softjourn.SJCoins.iOS/UI/Controllers/Main/DetailViewController.cs
+++ Softjourn.SJCoins.iOS/UI/Controllers/Main/DetailViewController.cs
@@ -7,8 +7,43 @@ namespace Softjourn.SJCoins.iOS.UI.Controllers
 	[Register("DetailViewController")]
 	public partial class DetailViewController : UIViewController
 	{
-		public DetailViewController()
+		#region Properties
+		#endregion
+
+		#region Constructor
+		public DetailViewController(IntPtr handle) : base(handle)
 		{
 		}
+		#endregion
+
+		#region Controller Life cycle
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+		}
+
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+
+			//Presenter.OnStartLoadingPage();
+		}
+
+		public override void ViewDidAppear(bool animated)
+		{
+			base.ViewDidAppear(animated);
+
+
+		}
+		#endregion
+
+		#region IDetailView implementation
+		#endregion
+
+		#region Private methods
+		#endregion
+
+		#region BaseViewController -> IBaseView implementation
+		#endregion
 	}
 }
