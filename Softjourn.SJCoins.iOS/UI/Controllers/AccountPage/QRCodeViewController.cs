47495d0b6b57b474950f04278d262cbae861f31a
Oleg Pankiv <oleg.pankiv@gmail.com>
2017-02-20 12:36:31 +0200

    created ui interface for QRCode functionality

---
+++ Softjourn.SJCoins.iOS/UI/Controllers/AccountPage/QRCodeViewController.cs
@@ -0,0 +1,25 @@
+using System;
+using Foundation;
+using UIKit;
+
+namespace Softjourn.SJCoins.iOS
+{
+	[Register("QRCodeViewController")]
+	public partial class QRCodeViewController : UIViewController
+	{
+		#region Properties
+		#endregion
+
+		#region Constructor
+		public QRCodeViewController(IntPtr handle) : base(handle)
+		{
+		}
+		#endregion
+
+		#region Controller Life cycle
+		#endregion
+
+		#region Private methods
+		#endregion
+	}
+}
---
+++ Softjourn.SJCoins.iOS/UI/Controllers/AccountPage/QRCodeViewController.designer.cs
@@ -0,0 +1,57 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Softjourn.SJCoins.iOS
+{
+	partial class QRCodeViewController
+	{
+		[Outlet]
+		UIKit.UITextField AmountTexfield { get; set; }
+
+		[Outlet]
+		UIKit.UILabel BalanceLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIBarButtonItem DoneButton { get; set; }
+
+		[Outlet]
+		UIKit.UIButton GenerateButton { get; set; }
+
+		[Outlet]
+		UIKit.UIImageView QRCodeImage { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (DoneButton != null) {
+				DoneButton.Dispose ();
+				DoneButton = null;
+			}
+
+			if (AmountTexfield != null) {
+				AmountTexfield.Dispose ();
+				AmountTexfield = null;
+			}
+
+			if (GenerateButton != null) {
+				GenerateButton.Dispose ();
+				GenerateButton = null;
+			}
+
+			if (BalanceLabel != null) {
+				BalanceLabel.Dispose ();
+				BalanceLabel = null;
+			}
+
+			if (QRCodeImage != null) {
+				QRCodeImage.Dispose ();
+				QRCodeImage = null;
+			}
+		}
+	}
+}
